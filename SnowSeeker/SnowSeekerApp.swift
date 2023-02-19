39608848f79c22641eaaaea6a6559adc5346ec3e
choijun0 <cjy00050005@gmail.com>
2023-02-19 20:11:01 +0900

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by 최준영 on 2023/02/19.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
